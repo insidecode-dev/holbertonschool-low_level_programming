42c933552da439020f7ee6d114b35cc0fb750038
root <trqtmhdv@gmail.com>
2023-10-18 00:49:32 -0700

    12. Software is eating the World updated

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -1,34 +1,22 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-  int i, j, k, d;
-  for (i = '0'; i <= '9'; i++)
-    {
-      for (j = '0'; j <= '9'; j++)
-	{
-	  for (k = i; k <= '8'; k++)
-	    {
-	      for (d = j+1; d <= '9'; d++)
-		{
-		  putchar(i);
-		  putchar(j);
-		  putchar(' ');
-		  putchar(k);
-		  putchar(d);
-		  if (!(i == '9' && j == '9' && k == '8' && d == '9'))
-		    {
-		      putchar(',');
- 		      putchar(' ');
-		    }
-		}
-	    }
-	}
-    }
-  putchar('\n');
-  return (0);
+int i, j;
+for (i = 0; i < 100; i++)
+{
+for (j = i + 1; j < 100; j++)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+if (i < 98) {
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return 0;
 }
