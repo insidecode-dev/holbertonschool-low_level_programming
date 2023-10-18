60d70e217dd6d39ce9b5f37ce4213d0d18b509f3
root <trqtmhdv@gmail.com>
2023-10-18 14:08:24 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game updated 4

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - checks if character is in lowercase
+ * @c: type of parameter is integer
+ * Description: This function checks if character is in lowercase
+ * Return: 1 if character is lowercase, 0 otherwise
+ **/
+int _islower(int c)
+{
+return (islower(c));
+}
