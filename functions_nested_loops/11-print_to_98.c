dadd12c9d22e01e9fe60e5df60b7385056baeec3
root <trqtmhdv@gmail.com>
2023-10-22 09:48:39 -0700

    11 last updated

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,76 +1,37 @@
 #include "main.h"
-#include <stdlib.h>
+
 /**
- * print_to_98 - prints all natural numbers from n to 98
- * @n: integer
- * Description: This function prints all natural numbers from n to 98
- **/
+ * print_to_98 - print for n to 98
+ * @n: first value
+ *
+ * Description: same
+ */
 void print_to_98(int n)
 {
-int i;
-if (n <= 98)
+if (n == 98)
 {
-for (i = n; i <= 98; i++)
+_putchar('0' + (n / 10));
+_putchar('0' + (n % 10));
+}
+if (n < 98)
 {
-if (i != n)
-{
-_putchar(',');
+do {
+n++;
+_putchar('0' + (n / 10));
+_putchar('0' + (n % 10));
 _putchar(' ');
-}
-if (i < 0)
-{
-_putchar('-');
-if (i <= -10)
-{
-_putchar((abs(i) / 10 + '0'));
-_putchar((abs(i) % 10 + '0'));
-}
-if (i >= 10 && i < 100)
-{
-_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
-}
-else if (i >= 0)
-{
-_putchar(i + '0');
-}
-if (i >= 100)
-{
-_putchar(i / 100 + '0');
-_putchar((i / 10) % 10 + '0');
-_putchar(i % 10 + '0');
-}
-else if (i <= -100)
-{
-_putchar('-');
-_putchar(i / 100 + '0');
-_putchar((i / 10) % 10 + '0');
-_putchar(i % 10 + '0');
-}
-}
-}
-}
-else
-{
-for (i = n; i >= 98; i--)
-{
-if (i != n)
-{
 _putchar(',');
+} while (n < 98);
+}
+if (n > 98)
+{
+do {
+n--;
+_putchar('0' + (n / 10));
+_putchar('0' + (n % 10));
 _putchar(' ');
-}
-if (i >= 10 && i < 100)
-{
-_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
-}
-if (i >= 100)
-{
-_putchar(i / 100 + '0');
-_putchar((i / 10) % 10 + '0');
-_putchar(i % 10 + '0');
-}
-}
+_putchar(',');
+} while (n > 98);
 }
 _putchar('\n');
 }
