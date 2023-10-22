ab00ccc853c01c54d8d9890dabd87c3ab9dcd2e7
root <trqtmhdv@gmail.com>
2023-10-21 22:38:16 -0700

    10. a + b updated 4

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,8 @@ void print_to_98(int n)
 {
 if (n <= 98)
 {
-for (int i = n; i <= 98; i++)
+int i;
+for (i = n; i <= 98; i++)
 {
 if (i < 10)
 {
@@ -28,18 +29,19 @@ _putchar(' ');
 }
 else
 {
-for (int i = n; i >= 98; i--)
+int j;
+for (j = n; j >= 98; j--)
 {
-if (i < 10)
+if (j < 10)
 {
-_putchar(i + '0');
+_putchar(j + '0');
 }
 else
 {
-_putchar(i / 10 + '0');
-_putchar(i % 10 + '0');
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
 }
-if (i > 98)
+if (j > 98)
 {
 _putchar(',');
 _putchar(' ');
