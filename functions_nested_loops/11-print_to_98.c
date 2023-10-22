cd5751bd58d85ab93a2bc0e2a2132874488ba8bc
root <trqtmhdv@gmail.com>
2023-10-22 02:24:48 -0700

    10. a + b updated 6

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,72 +6,69 @@
  **/
 void print_to_98(int n)
 {
+int i;
 if (n <= 98)
 {
-for (; n <= 98; n++)
+for (i = n; i <= 98; i++)
 {
-if (n == 98)
+if (i != n)
 {
-if (n >= 10)
-{
-_putchar(n / 10 + '0');
-_putchar(n % 10 + '0');
-}
-else
-{
-_putchar(n + '0');
-}
-_putchar('\n');
-break;
-}
-else
-{
-if (n >= 10)
-{
-_putchar(n / 10 + '0');
-_putchar(n % 10 + '0');
-}
-else
-{
-_putchar(n + '0');
-}
 _putchar(',');
 _putchar(' ');
 }
+if (i < 0)
+{
+_putchar('-');
+if (i <= -10)
+{
+_putchar((abs(i) / 10 + '0'));
+_putchar((abs(i) % 10 + '0'));
+}
+if (i >= 10 && i < 100)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+}
+else if (i >= 0)
+{
+_putchar(i + '0');
+}
+if (i >= 100)
+{
+_putchar(i / 100 + '0');
+_putchar((i / 10) % 10 + '0');
+_putchar(i % 10 + '0');
+}
+else if (i <= -100)
+{
+_putchar('-');
+_putchar(i / 100 + '0');
+_putchar((i / 10) % 10 + '0');
+_putchar(i % 10 + '0');
+}
 }
 }
 else
 {
-for (; n >= 98; n--)
+for (i = n; i >= 98; i--)
 {
-if (n == 98)
+if (i != n)
 {
-if (n >= 10)
-{
-_putchar(n / 10 + '0');
-_putchar(n % 10 + '0');
-}
-else
-{
-_putchar(n + '0');
-}
-_putchar('\n');
-break;
-}
-else
-{
-if (n >= 10)
-{
-_putchar(n / 10 + '0');
-_putchar(n % 10 + '0');
-}
-else
-{
-_putchar(n + '0');
-}
 _putchar(',');
 _putchar(' ');
 }
+if (i >= 10 && i < 100)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+}
+if (i >= 100)
+{
+_putchar(i / 100 + '0');
+_putchar((i / 10) % 10 + '0');
+_putchar(i % 10 + '0');
 }
 }
+}
+_putchar('\n');
 }
