8ff049103117f0c423c8985b562858bede4459c9
root <trqtmhdv@gmail.com>
2023-10-21 20:39:43 -0700

    9. Learn your times table updated 12

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@ void times_table(void)
 int i, j, product;
 for (i = 0; i <= 9; i++)
 {
-for (j = 0; j <= 9; j++)
+for (j = 1; j <= 9; j++)
 {
 product = i * j;
 if (j > 0)
@@ -17,11 +17,11 @@ if (j > 0)
 _putchar(',');
 _putchar(' ');
 }
-if (product < 10 && j != 0)
+if (product < 10)
 {
 _putchar(' ');
 }
-else
+else if(j != 0)
 {
 _putchar((product / 10) + '0');
 }
