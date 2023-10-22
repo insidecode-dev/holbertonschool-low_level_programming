9f9c03e9c52bfaeaa90044666432dc7571b196fd
root <trqtmhdv@gmail.com>
2023-10-21 22:17:27 -0700

    10. a + b updated

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
  * add - adds two integers and returns result
  * @a: first integer
- * @b: second integer 
+ * @b: second integer
  * Description: adds two integers and returns result
  * Return: Integer
  **/
