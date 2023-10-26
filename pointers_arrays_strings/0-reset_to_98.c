8770702ad29fb6367abec5bf059344d66a57c747
root <trqtmhdv@gmail.com>
2023-10-26 06:22:25 -0700

    0. 98 Battery st. updated 1

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include "main.h"
- /**
- * reset_to_98 - updates the value it points to to 98
- * @n: pointer to integer variable
- * Description: This function updates the value it points to to 98
- **/
 
+/**
+ * reset_to_98 -  updates the value it points to to 98
+ *
+ * @n: pointer to integer variable
+ *
+ * Return : nothing
+ */
 void reset_to_98(int *n)
 	{
 	  *n = 98;
