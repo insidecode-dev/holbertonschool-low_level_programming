fa814b367ab307a6a2d6ea4c1373864f355b2b07
root <trqtmhdv@gmail.com>
2023-10-27 12:31:18 -0700

    9. strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strcpy - copies string to another
+ * @dest: pointer to character
+ * @src: pointer to character
+ * Return: pointer to character
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
