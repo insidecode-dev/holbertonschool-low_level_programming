6007dce6422dc096ff58672908024054658f19cc
root <trqtmhdv@gmail.com>
2023-10-27 07:37:11 -0700

    5. A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes updated

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,25 +7,25 @@
  */
 void rev_string(char *s)
 {
-	int i;
+	size_t i;
 
 	i = strlen(s);
 	if (i % 2 == 0)
 	{
 		for (; i < (strlen(s) / 2); i++)
 		{
-			s[i] = (s[i] + '0') + (s[(strlen(s) - (i + 1)] + '0'));
-			s[(strlen(s) - (i + 1)] = s[i] - s[(strlen(s) - (i + 1)];
-			s[i] = s[i] - s[(strlen(s) - (i + 1)];
+			s[i] = (s[i] + '0') + (s[(strlen(s) - (i + 1))] + '0'));
+			s[(strlen(s) - (i + 1))] = s[i] - s[(strlen(s) - (i + 1))];
+			s[i] = s[i] - s[(strlen(s) - (i + 1))];
 		}
 	}
 	else
 	{
 		for (; i < ((strlen(s) / 2) - 1); i++)
 		{
-			s[i] = (s[i] + '0') + (s[(strlen(s) - (i + 1)] + '0'));
-			s[(strlen(s) - (i + 1)] = s[i] - s[(strlen(s) - (i + 1)];
-			s[i] = s[i] - s[(strlen(s) - (i + 1)];
+			s[i] = (s[i] + '0') + (s[(strlen(s) - (i + 1))] + '0'));
+			s[(strlen(s) - (i + 1))] = s[i] - s[(strlen(s) - (i + 1))];
+			s[i] = s[i] - s[(strlen(s) - (i + 1))];
 		}
 	}
 }
