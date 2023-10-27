8ccd5c41e2a98856e51892f1afd8f39c27c91ce1
root <trqtmhdv@gmail.com>
2023-10-26 20:17:45 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * _puts - prints string
+ * @str: The character pointer to string
+ * Description: prints string
+ * Return: Nothing
+ */
+void _puts(char *str)
+	{
+		puts(str);
+	}
