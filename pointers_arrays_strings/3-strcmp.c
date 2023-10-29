9fa81b6bf6a63b08a6f40ecf061756843504710f
root <trqtmhdv@gmail.com>
2023-10-29 01:40:44 -0700

    3. strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include <string.h>
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer to character
+ * @s2: pointer to character
+ * Return: On match 0, on unmatch .
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if(*s1 == *s2)
+		s1++, s2++;
+		else
+		return (*s1 - *s2);
+	}
+	return (0);
+}
