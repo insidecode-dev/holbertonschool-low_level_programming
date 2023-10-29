42980c554c727b804d8543d3820cbcc9337d65e6
root <trqtmhdv@gmail.com>
2023-10-29 11:47:34 -0700

    5. strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+/**
+ * _strstr -  a function that locates a substring.
+ * @haystack: pointer to character
+ * @needle: pointer to character
+ * Return:  pointer to character
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *startn = needle, *starth = haystack;
+
+	while (*haystack)
+	{
+		starth = haystack;
+		needle = startn;
+		while (*haystack == *needle)
+		{
+			haystack++;
+			needle++;
+		}
+
+		if (*needle == '\0')
+		return (haystack);
+		haystack = starth + 1;
+	}
+	return (NULL);
+}
