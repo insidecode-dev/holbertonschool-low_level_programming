056d0fee5165fb4d5b610301dd3162714c353dea
root <trqtmhdv@gmail.com>
2023-10-30 10:38:14 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints string with recursion
+ * @s: pointer to character
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+		return;
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
