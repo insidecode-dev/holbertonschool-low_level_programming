2bcd8183c9267e3abf41bd10296529f5048c010e
root <trqtmhdv@gmail.com>
2023-11-02 06:42:44 -0700

    2. The best argument against democracy is a five-minute conversation with the average voter updated 6

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,15 +9,22 @@
 int main(int argc, char *argv[])
 {
 	int result;
+	int result2;
 	char *word = "Exit";
 
+	result2 = 0;
 	if (argc == 3)
 	{
 		result = atoi(argv[1]) * atoi(argv[2]);
 		while (result)
 		{
-			_putchar(result % 10 + '0');
+			result2 = result2 * 10 + result % 10;
 			result /= 10;
+		}
+		while (result2)
+		{
+			_putchar(result2 % 10);
+			result2 /= 10;
 		}
 		_putchar('\n');
 		return (0);
