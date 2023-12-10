289128cd084a013b4e899c08107f7a79718636c8
root <trqtmhdv@gmail.com>
2023-12-10 11:06:26 -0800

    0. 0

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,8 +1,13 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
+#include <stdlib.h>
+#include <stddef.h>
+
+
 unsigned int binary_to_uint(const char *b);
+int check_valid_string(const char *b);
+int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
@@ -10,4 +15,5 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif /* HOLBERTON_H */
+
+#endif
