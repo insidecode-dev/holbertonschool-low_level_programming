f13550adc85d8a5065461068e396fbd29a50f212
root <trqtmhdv@gmail.com>
2023-12-17 16:02:26 -0800

    2. A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,50 +1,48 @@
 #include "dog.h"
+#include <stdlib.h>
 
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
 
 /**
- *_strlen - returns length of
- *a string
- *@str: string to be counted
- *Return: returns length of string
+ * _strlen - Finds the length of a string.
+ * @str: The string to be measured.
+ * Return: The length of the string.
  */
 int _strlen(char *str)
 {
 	int len = 0;
-	while (str)
-	len++;
 
+	while (*str++)
+	len++;
 	return (len);
 }
 
-
 /**
- *_strcopy - copy string pointed by src
- *into dest variable
- *@dest:buffer storing string copy
- *@src: buffer storing string to copy
- *Return:returns copied string
+ * _strcopy - Copies a string pointed to by src
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * Return: The pointer to dest.
  */
 char *_strcopy(char *dest, char *src)
 {
 	int index = 0;
 
-	for (; src[index] ; index++)
+	for (index = 0; src[index]; index++)
 	dest[index] = src[index];
 
 	dest[index] = '\0';
+
 	return (dest);
 }
 
-
-
-
 /**
- *new_dog - creates a new dog
- *@name: name of new dog
- *@age: age of new dog
- *@owner: owner of new dog
- *Return: returns NULL in case
- *of failure
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ * Return: The new struct dog.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
